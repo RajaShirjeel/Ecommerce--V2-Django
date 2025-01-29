cef7e9b897e15f761cceca69c04a0f753afe4ec3
Shirjeel <rajasarjais@gmail.com>
2025-01-29 22:20:46 +0530

    Added some responsiveness in navbar

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,13 +1,17 @@
 const categoryLink = document.querySelector('.category-link');
 const navCatContainer = document.querySelector('.nav__categories--container');
 const nav = document.querySelector('.nav');
-const searchIcon = document.querySelector('.nav-search-icon');
-const searchBar = document.querySelector('.nav__searchbar');
-const crossIcon = document.querySelector('.searchbar-icon');
-const overlay = document.querySelector('.nav__searchbar-overlay');
+const searchIcon = document.querySelectorAll('.nav-search-icon');
+const searchBar = document.querySelectorAll('.nav__searchbar');
+const crossIcon = document.querySelectorAll('.searchbar-icon');
+const overlay = document.querySelectorAll('.nav__searchbar-overlay');
 const cartIcon = document.querySelector('.nav-cart-icon');
 const hideCartIcon = document.querySelector('.cart-icon');
 const cartSlider = document.querySelector('.nav__cart__slider');
+const modalIcon = document.querySelector('.mobile__nav--icon');
+const hideModalIcon = document.querySelector('.modal-cancel');
+const modalContainer = document.querySelector('.mobile__nav--modal');
+const modalOverlay = document.querySelector('.modal-overlay');
 
 
 const showCategories = function () {
@@ -21,17 +25,33 @@ const hideCategories = function () {
 };
 
 const showSearchbar = function(){
-    searchBar.classList.remove('hide-searchbar');
-    searchBar.removeAttribute('inert');
-    overlay.classList.remove('hide-overlay');
-    overlay.removeAttribute('inert');
+    searchBar.forEach(bar => {
+        bar.classList.remove('hide-searchbar');
+    });
+    searchBar.forEach(bar => {
+        bar.removeAttribute('inert');
+    });
+    overlay.forEach(layer => {
+        layer.classList.remove('hide-overlay');
+    });
+    overlay.forEach(layer => {
+        layer.removeAttribute('inert')
+    });
 }
 
 const hideSearchbar = function(){
-    searchBar.classList.add('hide-searchbar');
-    searchBar.setAttribute('inert', '');
-    overlay.classList.add('hide-overlay');
-    overlay.setAttribute('inert', '');
+    searchBar.forEach(bar => {
+        bar.classList.add('hide-searchbar');
+    })
+    searchBar.forEach(bar => {
+        bar.setAttribute('inert', '');
+    })
+    overlay.forEach(layer => {
+        layer.classList.add('hide-overlay');
+    });
+    overlay.forEach(layer => {
+        layer.setAttribute('inert', '')
+    });
 }
 
 const showCart = function(){
@@ -44,11 +64,29 @@ const hideCart = function(){
     cartSlider.setAttribute('inert', '');
 }
 
+const showModal = function(){
+    modalContainer.classList.remove('hidden');
+    modalContainer.removeAttribute('inert');
+    modalOverlay.classList.remove('hide');
 
+}
+
+const hideModal = function(){
+    modalContainer.classList.add('hidden');
+    modalContainer.setAttribute('inert', '');
+    modalOverlay.classList.add('hide');
+}
+
+searchIcon.forEach(icon => {
+    icon.addEventListener('click', showSearchbar);
+})
+crossIcon.forEach(icon => {
+    icon.addEventListener('click', hideSearchbar);
+})
 
 categoryLink.addEventListener('mouseover', showCategories);
 nav.addEventListener('mouseleave', hideCategories);
-searchIcon.addEventListener('click', showSearchbar);
-crossIcon.addEventListener('click', hideSearchbar);
 cartIcon.addEventListener('click', showCart);
 hideCartIcon.addEventListener('click', hideCart);
+modalIcon.addEventListener('click', showModal);
+hideModalIcon.addEventListener('click', hideModal);
